5c2ba432bbf594595cfea008dc4097bdeec9161d
aniketsaste96 <90027213+aniketsaste96@users.noreply.github.com>
2022-01-11 10:19:06 +0530

    Update dailytask2.js

--- dailytask2.js
+++ dailytask2.js
@@ -1,16 +1,44 @@
 app.get("/movies/", async (request, response) => {
     const { language, rating } = request.query;
+    if (language) {
+        const movie = await client.db("b29wd").collection("movies").find({
+            $or: [
+                { "language": { "$in": [language] } }
 
-    const movie = await client.db("b29wd").collection("movies").find({
-        $or: [
-            { "language": { "$in": [language] } },
-            { "rating": { "$in": [+rating] } }
-        ]
-    }).toArray();
-
-    response.send(movie);
+            ]
+        }).toArray();
+        response.send(movie);
+    }
+    if (rating) {
+        const movie = await client.db("b29wd").collection("movies").find({
+            $or: [
+                { "rating": { "$in": [+rating] } }
+            ]
+        }).toArray();
+        response.send(movie);
+    }
+    else { response.send(movies) };
     console.log(request.query, language)
-    // response.send(movies);
-
-
 });
+
+
+
+
+
+
+// app.get("/movies/", async (request, response) => {
+//     const { language, rating } = request.query;
+
+//     const movie = await client.db("b29wd").collection("movies").find({
+//         $or: [
+//             { "language": { "$in": [language] } },
+//             { "rating": { "$in": [+rating] } }
+//         ]
+//     }).toArray();
+
+//     response.send(movie);
+//     console.log(request.query, language)
+//     // response.send(movies);
+
+
+// });
